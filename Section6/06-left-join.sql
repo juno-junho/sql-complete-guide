b36e7a1da41bd9e99214a6d49040288cd9d8103e
juno-junho <ssmm0205@naver.com>
2022-05-12 22:07:23 +0900

    INNER, OUTER JOIN & FOREIGN KEY CONSTRAINTS

---
+++ Section6/06-left-join.sql
@@ -0,0 +1,14 @@
+SELECT * 
+FROM users AS u
+LEFT JOIN addresses AS a ON a.id = u.address_id;
+
+-- inner join과 동일한 역할
+
+SELECT * 
+FROM addresses AS a
+LEFT JOIN users AS u ON a.id = u.address_id;
+
+SELECT * 
+FROM addresses AS a
+LEFT JOIN users AS u ON a.id = u.address_id
+INNER JOIN cities AS c ON c.id = a.city_id;
---
+++ Section6/13-create-tables.sql
@@ -0,0 +1,43 @@
+-- -- Postgresql
+-- CREATE TABLE addresses (
+--     id SERIAL PRIMARY KEY,
+--     street VARCHAR(300) NOT NULL,
+--     house_number VARCHAR(50) NOT NULL,
+--     city_id INT NOT NULL
+-- );
+
+-- CREATE TABLE users (
+--     id SERIAL PRIMARY KEY,
+--     first_name VARCHAR(300) NOT NULL,
+--     last_name VARCHAR(300) NOT NULL,
+--     email VARCHAR(300) NOT NULL,
+--     address_id INT REFERENCES addresses (id) ON DELETE CASCADE
+-- );
+
+-- CREATE TABLE cities (
+--     id SERIAL PRIMARY KEY,
+--     name VARCHAR(300) NOT NULL
+-- );
+
+
+-- Mysql
+CREATE TABLE addresses (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    street VARCHAR(300) NOT NULL,
+    house_number VARCHAR(50) NOT NULL,
+    city_id INT NOT NULL
+);
+
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(300) NOT NULL,
+    last_name VARCHAR(300) NOT NULL,
+    email VARCHAR(300) NOT NULL,
+    address_id INT,
+    FOREIGN KEY (address_id) REFERENCES addresses (id) ON DELETE CASCADE
+);
+
+CREATE TABLE cities (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(300) NOT NULL
+);
