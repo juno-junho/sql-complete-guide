c7c4dd7cd873852df2fdfe786bdb341868c668c4
juno-junho <ssmm0205@naver.com>
2022-05-17 22:35:28 +0900

    GROUP BY Keyword

---
+++ Section8/05-group-by.sql
@@ -0,0 +1,11 @@
+-- SELECT booking_date, SUM(num_guests)
+-- FROM bookings
+-- GROUP BY booking_date;
+
+-- SELECT DISTINCT booking_date, SUM(num_guests)
+-- FROM bookings;
+
+SELECT p.name, b.booking_date, SUM(b.num_guests), ROUND(AVG(amount_tipped),2)
+FROM payment_methods AS p
+INNER JOIN bookings AS b ON p.id = b.payment_id
+GROUP BY p.name, b.booking_date;
--- test.sql
+++ test.sql
@@ -1,1 +1,2 @@
-SELECT 'Hello World';
+SELECT booking_date AS date, SUM(num_guests) FROM bookings
+GROUP BY booking_date;
